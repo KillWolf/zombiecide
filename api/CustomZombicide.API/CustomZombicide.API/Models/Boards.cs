d76bbf52a16ea1f2c1f41eba973e957de2ceb272
Kenneth S. Mørck <ksm91er@gmail.com>
2019-09-01 18:28:46 +0200

    Added EF support and read the initial DB

---
+++ api/CustomZombicide.API/CustomZombicide.API/Models/Boards.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomZombicide.API.Models
+{
+    public partial class Boards
+    {
+        public Boards()
+        {
+            BoardTiles = new HashSet<BoardTiles>();
+        }
+        [Key]
+        public int BoardId { get; set; }
+        public string BoardName { get; set; }
+        public byte BoardHeight { get; set; }
+        public byte BoardWidth { get; set; }
+
+        public virtual ICollection<BoardTiles> BoardTiles { get; set; }
+    }
+}
