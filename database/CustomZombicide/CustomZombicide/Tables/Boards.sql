73fce031c884fd19ae2fb0c3511b839d3f2fe2a3
Kenneth S. Mørck <ksm91er@gmail.com>
2019-09-01 16:53:03 +0200

    Added .gitignore and boards

---
+++ database/CustomZombicide/CustomZombicide/Tables/Boards.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Boards]
+(
+	--Columns
+	[BoardId] INT IDENTITY NOT NULL,
+	[BoardName] VARCHAR(64) NOT NULL,
+	[BoardHeight] TINYINT NOT NULL,
+	[BoardWidth] TINYINT NOT NULL
+
+	--Keys
+	CONSTRAINT PK_Boards PRIMARY KEY (BoardId)
+
+	--Constraints
+)
